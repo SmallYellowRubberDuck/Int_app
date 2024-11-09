30da22c21db692e9499571cd04d8e3b8d079ae56
SmallYellowRubberDuck <stepangordeev548@gmail.com>
2024-11-10 02:39:44 +0300

    add further learning

--- app.py
+++ app.py
@@ -26,17 +26,23 @@ class VideoProcessingApp:
         self.root.title("Video Processing App")
         self.root.geometry("1280x800")
         self.root.configure(bg="#F4F6F9")
+        
+        # Центрируем главное окно
+        self.center_window(self.root)
 
         self.video_files = []
 
         # Меню
         self.menu = tk.Menu(root)
         root.config(menu=self.menu)
-        
+
         # Подменю для выбора конфигурации ПК
         self.pc_config_menu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="PC Configuration", menu=self.pc_config_menu)
-
+        # Кнопка для дообучения модели
+        self.further_train_button = tk.Button(root, text="Train Model", command=self.load_files_window, relief="raised", width=30, height=2, bg="#FF9800", fg="white", font=("Arial", 12, "bold"), activebackground="#FB8C00", activeforeground="white")
+        self.further_train_button.pack(pady=15)
+        
         # Кнопка для загрузки видео
         self.upload_button = tk.Button(root, text="Upload Video", command=self.upload_video, relief="raised", width=20, height=2, bg="#8320F5", fg="white", font=("Arial", 12, "bold"), activebackground="#6C94DC", activeforeground="white")
         self.upload_button.pack(pady=15)
@@ -62,7 +68,6 @@ class VideoProcessingApp:
         # Устанавливаем стиль для Treeview
         self.tree.tag_configure("evenrow", background="#F1F1F1")
         self.tree.tag_configure("oddrow", background="#FFFFFF")
-
         self.tree.pack(fill=tk.BOTH, expand=True, pady=10)
 
         # Конфигурации по умолчанию
@@ -75,6 +80,63 @@ class VideoProcessingApp:
         self.ram_menu_item = self.pc_config_menu.add_command(label=f"RAM: {self.ram.get()}", command=self.select_ram)
         self.gpu_menu_item = self.pc_config_menu.add_command(label=f"Graphics Card: {self.gpu.get()}", command=self.select_gpu)
 
+    def load_files_window(self):
+            top = tk.Toplevel(self.root)
+            top.title("Choose video folder and markup excel file")
+            top.grab_set()
+            # Метки и кнопки для выбора видео папки и xlsx файла
+            video_folder_label = tk.Label(top, text="Select Video Folder:", font=("Arial", 12))
+            video_folder_label.pack(padx=10, pady=5)
+
+            self.video_folder_path = tk.StringVar()
+            video_folder_button = tk.Button(top, text="Browse", command=self.select_video_folder, width=20, height=2, bg="#4CAF50", fg="white", font=("Arial", 12, "bold"))
+            video_folder_button.pack(pady=5)
+
+            self.xlsx_file_path = tk.StringVar()
+            xlsx_label = tk.Label(top, text="Select Excel File:", font=("Arial", 12))
+            xlsx_label.pack(padx=10, pady=5)
+            
+            xlsx_button = tk.Button(top, text="Browse", command=self.select_xlsx_file, width=20, height=2, bg="#2196F3", fg="white", font=("Arial", 12, "bold"))
+            xlsx_button.pack(pady=5)
+            #Создадим пробел
+            empty_space = tk.Label(top, text="", height=1)
+            empty_space.pack(pady=10)
+            # Кнопка для подтверждения окончания выбора
+            close_button = tk.Button(top, text="Confirm", command=lambda: self.close_train_window(top), width=20, height=2, bg="#FF9800", fg="white", font=("Arial", 12, "bold"))
+            close_button.pack(pady=10)
+            # Кнопка отмены
+            cancel_button = tk.Button(top, text="Cancel", command = top.destroy, width=20, height=2, bg="#FF5722", fg='white', font=("Arial", 12, "bold"))
+            cancel_button.pack(pady=10)
+            self.center_window(top)
+    def select_video_folder(self):
+        folder_path = filedialog.askdirectory(title="Select Video Folder")
+        if folder_path:
+            self.video_folder_path.set(folder_path)
+            
+    def select_xlsx_file(self):
+        file_path = filedialog.askopenfilename(title="Select Excel File", filetypes=[("Excel files", "*.xlsx")])
+        if file_path:
+            self.xlsx_file_path.set(file_path)
+    def close_train_window(self, top):
+        if self.video_folder_path.get() and self.xlsx_file_path.get():
+            messagebox.showinfo("Info", f"You selected {self.video_folder_path.get()} as video folder\n and \n{self.xlsx_file_path.get()} as markup file")
+            top.grab_release()
+            top.destroy()
+        else:
+            messagebox.showwarning("Warning", "Please, select both video folder and Excel file")
+    
+    def center_window(self, window):
+        window.update_idletasks()
+        width = window.winfo_width()
+        height = window.winfo_height()
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        
+        # Вычисляем позицию для центрирования окна
+        position_top = int(screen_height / 2 - height / 2)
+        position_left = int(screen_width / 2 - width / 2)
+        
+        window.geometry(f'{width}x{height}+{position_left}+{position_top}')
     def get_processor_info(self):
         processor_info = platform.processor()
         if 'Intel' in processor_info:
@@ -88,7 +150,8 @@ class VideoProcessingApp:
 
     def get_memory_info(self):
         mem = psutil.virtual_memory()
-        self.ram = tk.StringVar(value=f"{math.ceil(mem.total/1024**3)} GB")
+        self.ram = tk.StringVar(value=f"{math.ceil(mem.total / 1024 ** 3)} GB")
+
     def check_cuda_availability(self):
         if torch.cuda.is_available():
             self.gpu = tk.StringVar(value="With CUDA")
@@ -110,7 +173,10 @@ class VideoProcessingApp:
     def show_option_menu(self, title, variable, options):
         top = tk.Toplevel(self.root)
         top.title(title)
-
+        
+        # Центрируем всплывающее окно относительно главного окна
+        self.center_window(top)
+        
         combobox = ttk.Combobox(top, textvariable=variable, values=options, state="readonly", font=("Arial", 12), width=20)
         combobox.pack(padx=20, pady=20)
         combobox.set(variable.get())  # Устанавливаем текущее значение
@@ -123,7 +189,7 @@ class VideoProcessingApp:
             self.gpu_menu_item = self.pc_config_menu.add_command(label=f"Graphics Card: {self.gpu.get()}", command=self.select_gpu)
             messagebox.showinfo("Selected", f"You selected: {variable.get()}")
 
-        select_button = tk.Button(top, text="Select", command=on_select, width=15, height=2, bg="#4CAF50", fg="white", font=("Arial", 12, "bold"), activebackground="#45a049", activeforeground="white")
+        select_button = tk.Button(top, text="Select", command=on_select, width=15, height=2, bg="#6C94DC", fg="white", font=("Arial", 12, "bold"), activebackground="#A9C8FF", activeforeground="white")
         select_button.pack(pady=10)
 
     def upload_video(self):
@@ -135,20 +201,16 @@ class VideoProcessingApp:
         if not self.video_files:
             messagebox.showwarning("Warning", "No video files uploaded!")
             return
-
         results = []
         for video in self.video_files:
             result = ml_model(video)
             results.append(result)
-
         # Сохраняем результаты в таблице
         self.results_table = pd.DataFrame(results)
         messagebox.showinfo("Info", "Processing complete!")
-
         # Очищаем Treeview перед добавлением новых данных
         for item in self.tree.get_children():
             self.tree.delete(item)
-
         # Добавляем новые данные в Treeview
         for index, row in self.results_table.iterrows():
             tag = "oddrow" if index % 2 == 0 else "evenrow"
@@ -158,7 +220,6 @@ class VideoProcessingApp:
         if self.results_table is None:
             messagebox.showwarning("Warning", "No results to save!")
             return
-
         file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
         if file_path:
             self.results_table.to_excel(file_path, index=False)
